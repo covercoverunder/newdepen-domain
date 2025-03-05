0bbf8fb72228138a7a68de657eb7e197896f9bbe
mCode117 <michael.v3365@gmail.com>
2025-03-04 19:24:34 -0500

    Removed infinite loop

--- src/main/java/newdepen.java
+++ src/main/java/newdepen.java
@@ -4,8 +4,10 @@ public class newdepen {
     //THIS IS DUE IN 2 MINUTES OR YOU'RE FIRED
 
     public static void main(String[] args ) {
-        while(true) {
+        int z = 0;
+        while(z<10) {
             System.out.println("???");
+            z++;
         }
     }
 }
