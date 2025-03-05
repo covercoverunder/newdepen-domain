dbfed5b49382e09179fe7eee4e8d1ccc0792e878
mCode117 <michael.v3365@gmail.com>
2025-03-04 19:27:39 -0500

    Moved print statement into main

--- src/main/java/newdepen.java
+++ src/main/java/newdepen.java
@@ -1,9 +1,9 @@
 public class newdepen {
-    System.out.println("hello"); //Do better
-
+    
     //THIS IS DUE IN 2 MINUTES OR YOU'RE FIRED
-
-    public static void main(String[] args ) {
+    
+    public static void main(String[] args) {
+        System.out.println("hello"); //Do better
         int z = 0;
         while(z<10) {
             System.out.println("???");
