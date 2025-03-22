d77817aa2c24102e15a00e11f6082fcea14eaff4
Snehehe <127688643+Snehehe@users.noreply.github.com>
2025-03-22 15:16:57 -0400

    Update newdepen.java

--- src/main/java/newdepen.java
+++ src/main/java/newdepen.java
@@ -3,7 +3,7 @@ public class newdepen {
     //THIS IS DUE IN 2 MINUTES OR YOU'RE FIRED
     
     public static void main(String[] args) {
-        System.out.println("hello"); //Do better
+        System.out.println("hello world"); //Do better
         int z = 0;
         while(z<10) {
             System.out.println("???");
