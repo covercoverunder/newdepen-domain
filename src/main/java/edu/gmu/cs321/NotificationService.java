9999fc425edfcf0de6f61e707d328d0fe7f679d8
covercoverunder <covercoverunder@gmail.com>
2025-03-22 22:02:47 -0400

    junit test

---
+++ src/main/java/edu/gmu/cs321/NotificationService.java
@@ -0,0 +1,7 @@
+package edu.gmu.cs321;
+
+public class NotificationService {
+    public static void main(String[] args) {
+        System.out.println("bye world");
+    }
+}
---
+++ src/main/java/edu/gmu/cs321/newdepen.java
@@ -0,0 +1,6 @@
+package edu.gmu.cs321;
+public class newdepen {
+    public static boolean testVar(String var) {
+        return true;
+    }
+}
